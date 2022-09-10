27001613b0b75e6fe7de65537015fd2e65362174
rafix923 <rafaellopes.dev@gmail.com>
2022-09-09 22:00:48 -0300

    Player features

--- blackjack/script.js
+++ blackjack/script.js
@@ -2,30 +2,30 @@
 Além de gerar a pontuação final */
 
 let blackjackGame = {
-    'you': {
-        'scoreSpan': '#your-blackjack-result',
-        'div': '#your-box',
-        'box-size': '.flex-blackjack-row-2 div',
-        'socre': 0
+    you: {
+        scoreSpan: '#your-blackjack-result',
+        div: '#your-box',
+        boxSize: '.flex-blackjack-row-2 div',
+        score: 0
     },
 
-    'dealer': {
-        'score-span': '#dealer-blackjack-result',
-        'div': '#dealer-box',
-        'box-size': '.flex-blackjack-row-2 div',
-        'score': 0
+    dealer: {
+        scoreSpan: '#dealer-blackjack-result',
+        div: '#dealer-box',
+        boxSize: '.flex-blackjack-row-2 div',
+        score: 0
     },
 
-    'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
+    cards: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
 
-    'cardsMap': { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11] },
+    cardsMap: { '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11] },
 
-    'wins': 0,
-    'losses': 0,
-    'draws': 0,
-    'isStand': false,
-    'isTurnsOver': false,
-    'pressOnce': false,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+    isStand: false,
+    isTurnsOver: false,
+    pressOnce: false,
 }
 
 const YOU = blackjackGame['you'];
@@ -37,15 +37,15 @@ let windowHeight = window.screen.height;
 let winner;
 
 // Evento listener para os botões
-document.querySelector("#blackjack-hit-button")
-.addEventListener("click", blackjackHit);
+document.querySelector("#hit-button").addEventListener("click", blackjackHit);
 
 // Variável para guardar o resultado da função randomCard
 function blackjackHit() {
     if (blackjackGame['isStand'] === false) {
         let card = randomCard();
-        console.log(card);
         showCard(card, YOU);
+        updateScore(card, YOU);
+        showScore(YOU)
     }
 }
 
@@ -81,3 +81,27 @@ function heightSize() {
         return window.screen.height * 0.15;
     }
 }
+
+function updateScore(card, activePlayer) {
+    if (card === 'A') {
+        if (activePlayer['score'] + blackjackGame['cardsMap'][card][1] <= 21) {
+            activePlayer['score'] += blackjackGame['cardsMap'][card][1];
+        } else {
+            activePlayer['score'] += blackjackGame['cardsMap'][card][0];
+        }
+    } else {
+        activePlayer['score'] += blackjackGame['cardsMap'][card];
+    }
+    console.log(activePlayer);
+}
+
+function showScore(activePlayer) {
+    if (activePlayer['score'] > 21) {
+        document.querySelector(activePlayer['scoreSpan']).textContent = 'Bust!';
+        document.querySelector(activePlayer['scoreSpan']).style.color = 'red';
+    }else{
+        document.querySelector(activePlayer['scoreSpan']).textContent = activePlayer['score'];
+    }
+}
+
+// Funções para o Croupier
