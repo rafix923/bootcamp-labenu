a0f1cbf3080955a83cef71861c07b0f38bd43cfb
rafix923 <rafaellopes.dev@gmail.com>
2022-10-08 08:07:44 -0300

    Se adicionar esse commit, adicionará a condição para o cálculo do IMC

--- calculadora-IMC/script.js
+++ calculadora-IMC/script.js
@@ -1,6 +1,30 @@
-const calculateBMI = () => {
-    const bmi = peso / (altura * altura);
-    bmi = result.toFixed(2);
-    return bmi;
-}
-calculateBMI(73 * 1,23)
+const heightInput = document.querySelector(".height-input");
+const weightInput = document.querySelector(".weight-input");
+const calculateButton = document.querySelector(".calculate");
+const result = document.querySelector(".result");
+const statement = document.querySelector(".statement");
+let height, weight, BMI;
+
+calculateButton.addEventListener("click", () => {
+    height = heightInput.value;
+    weight = weightInput.value;
+    BMI = weight / (height ** 2);
+    result.innerText = BMI;
+
+
+    if (BMI < 18.5) {
+        statement.innerText = "Você está abaixo do peso ideal. Consulte um médico para fechar um melhor diagnóstico.";
+    }
+    else if ((BMI > 18.5) && (BMI < 24.9)) {
+        statement.innerText = "Seu IMC está dentro da normalidade.";
+    } else if ((BMI > 25) && (BMI < 29.9)) {
+        statement.innerText = "Você está com sobrepeso. Consulte um médico para fechar um melhor diagnóstico.";
+    } else if ((BMI > 30) && (BMI < 40)) {
+        statement.innerText = "Você está com obesidade. Consulte um médico para fechar um melhor diagnóstico.";
+    } else {
+        statement.innerText = "Você está com obesidade grave. Consulte um médico para fechar um melhor diagnóstico.";
+    }
+});
+
+
+
