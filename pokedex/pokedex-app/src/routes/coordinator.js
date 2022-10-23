575728b7bc16676d058b4579978f35ffcb91d6f4
Rafael Lopes <rafaellopes.dev@gmail.com>
2022-10-23 14:12:10 -0300

    Delete pokedex directory

--- pokedex/pokedex-app/src/routes/coordinator.js
+++
@@ -1,6 +0,0 @@
-
-export const goToPokemonListScreen = (navigate) => { navigate("/") };
-
-export const goToPokedexScreen = (navigate) => { navigate("/pokedex") };
-
-export const goToPokemonDetailScreen = (navigate) => { navigate("pokemon/:name") };
